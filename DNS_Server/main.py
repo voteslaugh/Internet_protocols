6c3a0d47d3d30a1d33a4b8f67e4fbbb122fffc1c
voteslaugh <usualvlad@gmail.com>
2023-05-26 16:52:12 +0500

    добавлена обработка ошибок

--- DNS_Server/main.py
+++ DNS_Server/main.py
@@ -1,11 +1,13 @@
 from app import Server
+import logging
 
 
 def main():
     try:
         Server().run()
-    except Exception:
-        pass
+    except Exception as e:
+        logging.basicConfig(filename='app.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.error(e, exc_info=True)
 
 
 if __name__ == "__main__":
